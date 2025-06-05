10e82cb25c153645f44dd7f5ef82bca95377f758
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-06-05 18:20:45 +0700

    server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+const findMax = (numbers: number[]): string => {
+    if (numbers.length === 0) {
+    return "No numbers ";
+}
+    const maxNumber = Math.max(...numbers);
+    return maxNumber.toString();
+};
+
+const result = findMax([1, 2, 3, 4, 5]);
+console.log(result,'type of result',typeof result); // Output: "5"
